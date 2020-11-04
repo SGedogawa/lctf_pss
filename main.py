9e0fe286ed638f434c653bd6f42848f12cccc4b9
RYO SUGIYAMA <rs0723new@gmail.com>
2020-11-05 01:42:36 +0900

    first commit by RYO SUGIYAMA 201105

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from psslib import make_hyper_spectrum_cube as pss
+from psslib import make_spectrum_cube as sc
 
 
 if __name__ == "__main__":
-    spectrum_cube_from_csv = pss.MakeSpectrumCube().from_csv()
-    spectrum_cube_from_bmp = pss.MakeSpectrumCube().from_bmp()
+    spectrum_cube_from_csv = sc.MakeSpectrumCube().from_csv()
+    spectrum_cube_from_bmp = sc.MakeSpectrumCube().from_bmp()
